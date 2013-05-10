0cc5e8e937dedb8c25a77d4a37b4a04548325fbd
huzhifeng <huzhifeng@ymail.com>
2013-05-10 12:34:05 +0800

    Add RSS subscription

--- routes/rss.js
+++ routes/rss.js
@@ -1,22 +1,18 @@
-
-/*
- * home page.
- */
+// Refer to https://github.com/dylang/node-rss
 var util = require('util');
 var RSS = require('rss');
 
 var News = require('../models/news');
 var CONFIG = require('config').Config;
 
-
 var index = function (req, res, next) {
   var feed = new RSS({
     title: CONFIG.siteName,
-    description: '看点,精挑细选,必须好看!',
-    feed_url: 'http://kandian.huzhifeng.com/rss',
-    site_url: 'http://kandian.huzhifeng.com',
-    image_url: 'http://kandian.huzhifeng.com/img/kandian.png'
-    // author: 'Dylan Greene'
+    description: '看点---有看点，更精彩！网罗手机客户端精品栏目',
+    feed_url: 'http://huzhifeng.com/rss',
+    site_url: 'http://huzhifeng.com',
+    image_url: 'http://huzhifeng.com/kandian.png',
+    author: 'huzhifeng'
   });
 
   /* loop over data and add to feed */
@@ -26,22 +22,22 @@ var index = function (req, res, next) {
         var title = news.title;
         var docid = news.docid;
         var digest = util.format('<p><img src="%s" alt="%s" /></p><p>%s ...</p>',
-          (news.cover || 'http://kandian.huzhifeng.com/img/noImg.gif'), title, news.digest);
+          (news.cover || 'http://huzhifeng.com/img/noImg.gif'), title, news.digest);
         feed.item({
           title: news.title,
           description: digest,
-          url: 'http://kandian.huzhifeng.com/news/' + docid, // link to the item
+          url: 'http://huzhifeng.com/news/' + docid, // link to the item
+          author: 'huzhifeng',
           date: news.ptime // any format that js Date can parse.
         });
-      });
+      });//forEach
 
       res.set('Content-Type', 'application/xml');
       return res.send(feed.xml());
     } else {
       next(new Error(err.message));
     }
-  });
-
+  });//News.findLimit
 };
 
 exports.index = index;
